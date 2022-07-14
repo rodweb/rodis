affec71f5d83a23238afc9511ba490f248dbbdd4
Rodrigo Campos <rod.apd@gmail.com>
2022-07-13 22:12:42 -0300

    complete 2nd stage

--- app/server.go
+++ app/server.go
@@ -7,15 +7,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	var pong = []byte("+PONG\n")
+	conn.Write(pong)
 }
